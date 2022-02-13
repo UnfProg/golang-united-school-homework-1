f14d32e39f9d7689df38d42bdd88c85e18691a8b
Unflat <unflat.morgan@gmail.com>
2022-02-13 14:37:42 +0700

    Homework +- Done yet

--- solution.go
+++ solution.go
@@ -3,6 +3,5 @@ package solution
 import "github.com/kyokomi/emoji"
 
 func GetMessage() string {
-	str := emoji.Sprintf("Hello :world_map:!")
-	return str
+	return emoji.Sprintf("Hello :world_map:!")
 }
